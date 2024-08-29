dd4f5948e85bbcd925c7b4056d7252c857ca4c84
FernandaOchoa <monc_sip@hotmail.com>
2024-08-29 17:43:56 +0000

    add: solution challenge until 2

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,15 @@ import Link from "next/link";
 import dbConnect from "../lib/dbConnect";
 import Pet from "../models/Pet";
 
-// TODO: Import Hours component
+// Import Hours component
+import Hours from "../components/Hours";
 
 const Index = ({ pets }) => {
 
   return (
     <>
-
-      {/* TODO: Display Hours component */}
+      {/* Display Hours component */}
+      <Hours />
 
       {/* Create a card for each pet */}
       {pets.map((pet) => (
